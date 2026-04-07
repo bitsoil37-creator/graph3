f80ca75a3e5df9659c9a2b4d355e8e084450c7c7
bitsoil37-creator <bitsoil37@gmail.com>
2026-04-07 20:50:08 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,6 @@
 const url = new URL(window.location.href);
 const user = url.searchParams.get("user");
 const month = url.searchParams.get("month");
-let month = url.searchParams.get("month");
-
-
 const elements = url.searchParams.get("elements")?.split(",").map(e => e.trim().toLowerCase()) || [];
 
 // Firebase URL
